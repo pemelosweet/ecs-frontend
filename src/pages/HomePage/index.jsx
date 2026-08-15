920c3d7929c68098b88a1e2ace65c90b8d3f2f5a
liujiaxin <liujiaxin@ecareyun.com>
2026-08-15 16:48:47 +0800

    11

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,16 +1,5 @@
-import { Card, Typography } from 'antd'
-
-const { Title, Paragraph } = Typography
+import { Card } from 'antd'
 
 export default function HomePage() {
-  return (
-    <Card>
-      <Typography>
-        <Title level={3}>欢迎使用管理系统</Title>
-        <Paragraph>
-          这是一个前后端分离的示例项目：前端 React + Ant Design，后端 FastAPI + SQLite。
-        </Paragraph>
-      </Typography>
-    </Card>
-  )
+  return <Card>111</Card>
 }
